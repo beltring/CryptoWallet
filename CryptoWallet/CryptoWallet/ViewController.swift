4c02788ef455fb60129418605a53f6fa012e2c4f
Pavel Boltromyuk <pavel.boltromyuk99@gmail.com>
2023-03-17 10:49:51 +0300

    create project

---
+++ CryptoWallet/CryptoWallet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CryptoWallet
+//
+//  Created by Pavel Boltromyuk on 17.03.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
